dc97a315a672b9f0fa7cbd12184adc1a3acbb52c
saulojustiniano1 <saulojustiniano1@gmail.com>
2025-04-16 19:17:02 -0300

    refactor: simplificar a navegação e a implementação da barra de progresso no CadastroLayout

--- apps/web/src/app/(private)/espaco/cadastro/layout.tsx
+++ apps/web/src/app/(private)/espaco/cadastro/layout.tsx
@@ -1,17 +1,13 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import { Home } from 'lucide-react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { ArrowLeft } from 'lucide-react';
+import { usePathname, useRouter } from 'next/navigation';
 
 export default function CadastroLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
   const etapas = [
     '/espaco/cadastro/1',
     '/espaco/cadastro/2',
@@ -19,28 +15,38 @@ export default function CadastroLayout({
     '/espaco/cadastro/4',
     '/espaco/cadastro/5',
   ];
+  const pathname = usePathname();
+  const router = useRouter();
   const etapaAtual = etapas.indexOf(pathname);
+  const etapaAnterior = etapaAtual > 0 ? etapas[etapaAtual - 1] : null;
   const progressPercentage = ((etapaAtual + 1) / etapas.length) * 100;
 
+  const handleVoltar = () => {
+    if (etapaAnterior) {
+      router.push(etapaAnterior);
+    } else {
+      router.push('/');
+    }
+  };
+
   return (
-    <div className="flex bg-[#F3FBF9] text-black relative overflow-hidden min-h-screen">
+    <div
+      className={`flex bg-[#DDF0EF] dark:bg-zinc-900 relative overflow-hidden min-h-screen transition-colors`}
+    >
       <div className="w-[80px] flex flex-col items-center py-4 shrink-0">
-        <Link href="/">
-          <Button
-            variant="outline"
-            size="icon"
-            className="border-[#2C7DA0] bg-transparent cursor-pointer"
-          >
-            <Home className="text-[#2C7DA0]" />
-          </Button>
-        </Link>
-        <span className="sr-only">Voltar</span>
+        <button
+          onClick={handleVoltar}
+          className={`mt-5 p-2 rounded-full border border-[#2C7DA0] dark:border-gray-400 dark:text-gray-400 dark:hover:bg-gray-400 dark:hover:text-black text-[#2C7DA0]  hover:bg-[#2C7DA0] hover:text-white transition-colors duration-200 cursor-pointer`}
+          aria-label="Voltar"
+        >
+          <ArrowLeft size={20} />
+        </button>
 
         {/* Barra de progresso */}
         <div className="flex-1 flex flex-col justify-center mt-4">
-          <div className="w-2 h-3/4 bg-gray-200 shadow-2xl relative overflow-hidden rounded-full">
+          <div className="w-2 h-3/4 bg-gray-50 dark:bg-gray-400 shadow-2xl relative overflow-hidden rounded-full">
             <div
-              className="absolute top-0 left-0 w-full bg-[#2C7DA0] transition-all duration-300"
+              className="absolute top-0 left-0 w-full bg-[#2C7DA0] dark:bg-zinc-800 transition-all duration-300"
               style={{ height: `${progressPercentage}%` }}
             />
           </div>
