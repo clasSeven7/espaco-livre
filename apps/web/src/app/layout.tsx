e06dbd0a8b899492cc5503ce77cbb905ba0dd7fd
saulojustiniano1 <saulojustiniano1@gmail.com>
2025-04-28 11:04:36 -0300

    feat: adicionar links de favicon para suporte a temas claros e escuros

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -24,6 +24,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" suppressHydrationWarning>
+      <head>
+        <link
+          id="favicon-light"
+          rel="icon"
+          href="/favicon_claro.ico"
+          media="(prefers-color-scheme: light)"
+        />
+        <link
+          id="favicon-dark"
+          rel="icon"
+          href="/favicon_escuro.ico"
+          media="(prefers-color-scheme: dark)"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-100 dark:bg-zinc-900`}
       >
