a4ea6ea6cba2382abd993669a61acf288f2d77b9
saulojustiniano1 <saulojustiniano1@gmail.com>
2025-04-28 11:04:00 -0300

    feat: refatorar componente de login para simplificar a estrutura e melhorar a legibilidade

--- apps/web/src/app/(public)/login/page.tsx
+++ apps/web/src/app/(public)/login/page.tsx
@@ -31,20 +31,18 @@ interface FormData {
 
 export default function Login() {
   const router = useRouter();
-  const [formData, setFormData] = useState<FormData>({
-    nome_usuario: '',
-    senha: '',
-  });
   const [isLoading, setIsLoading] = useState(false);
   const [isSaved, setIsSaved] = useState(false);
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState<FormData>({
+    nome_usuario: '',
+    senha: '',
+  });
 
   useEffect(() => {
     const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-      setIsDarkMode(true);
-    }
+    if (savedTheme === 'dark') setIsDarkMode(true);
   }, []);
 
   const toggleTheme = () => {
@@ -54,23 +52,16 @@ export default function Login() {
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     setIsLoading(true);
-
     try {
       const response = await api.post('/auth/login', formData);
-
-      // Salva o token e os dados do usuário nos cookies
       Cookies.set('token', response.data.token);
       Cookies.set('user', JSON.stringify(response.data.usuario));
-
       toast.success('Login realizado com sucesso!');
       router.push('/');
     } catch (error: unknown) {
@@ -85,218 +76,30 @@ export default function Login() {
   };
 
   return (
-    <>
+    <div className="flex h-screen">
+      <div className="hidden lg:flex w-1/2 relative opacity-90">
+        <Image
+          src="/bg_login.png"
+          alt="Imagem da sala"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+
       <div
-        className={`flex flex-col items-center justify-center min-h-screen relative px-4 overflow-hidden transition-colors duration-300 ${
-          isDarkMode ? 'bg-[#1f1f1f]' : 'bg-[#DDF0EF]'
+        className={`relative flex flex-col w-full lg:w-1/2 justify-center items-center px-8 ${
+          isDarkMode
+            ? 'bg-gradient-to-tl from-[#212a30] to-[#161c20]'
+            : 'bg-gradient-to-tl from-[#1178B9] to-[#0d4f7d]'
         }`}
       >
-        <Image
-          src="/background.png"
-          alt="Fundo"
-          width={1920}
-          height={1080}
-          priority
-          className="absolute top-0 left-0 z-0 opacity-10 object-cover"
-        />
-
-        {/* Logo + Título */}
-        <div className="flex flex-col sm:flex-row items-center justify-center gap-4 mb-10 max-w-full z-10">
-          <div className="relative w-16 sm:w-20 md:w-24 lg:w-28 aspect-[103.8/98.49]">
-            <Image
-              src={isDarkMode ? '/icone_branco.png' : '/icone_preto.png'}
-              alt="Logo"
-              width={100}
-              height={100}
-              priority
-              className="absolute"
-            />
-          </div>
-          <span
-            className={`text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-center sm:text-left ${
-              isDarkMode ? 'text-white' : 'text-zinc-950'
-            }`}
-          >
-            Login
-          </span>
-        </div>
-
-        {/* Formulário */}
-        <form onSubmit={handleLogin} className="w-full max-w-[600px] z-10 px-4">
-          <div className="mb-8 relative">
-            <User className="absolute left-3 top-1/2 transform -translate-y-1/2 text-zinc-50" />
-            <Input
-              name="nome_usuario"
-              value={formData.nome_usuario}
-              onChange={handleInputChange}
-              placeholder="Digite aqui seu login!"
-              className={`py-6 rounded-lg border-0 pl-10 pr-10 text-white placeholder:text-white/40 focus:outline-none
-            transition-colors duration-300 ${
-              isDarkMode
-                ? 'bg-[#333333] focus:ring-2 focus:ring-white/40'
-                : 'bg-[#1178B9] focus:ring-2 focus:ring-[#0C5C8C]'
-            }`}
-              required
-            />
-          </div>
-          <div className="mb-8 relative">
-            <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-zinc-50" />
-            <Input
-              type={showPassword ? 'text' : 'password'}
-              name="senha"
-              value={formData.senha}
-              onChange={handleInputChange}
-              placeholder="Digite aqui a sua senha!"
-              className={`py-6 rounded-lg border-0 pl-10 pr-10 w-full text-white placeholder:text-white/40 focus:outline-none
-            transition-colors duration-300 ${
-              isDarkMode
-                ? 'bg-[#333333] focus:ring-2 focus:ring-white/40'
-                : 'bg-[#1178B9] focus:ring-2 focus:ring-[#0C5C8C]'
-            }`}
-              required
-            />
-
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white hover:text-white/70 cursor-pointer"
-              aria-label={showPassword ? 'Esconder senha' : 'Mostrar senha'}
-            >
-              {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-            </button>
-          </div>
-          <Button
-            type="submit"
-            className="w-full py-5 cursor-pointer text-lg font-bold text-white hover:bg-zinc-800 bg-black !opacity-100"
-            disabled={isLoading}
-          >
-            {isLoading ? (
-              <>
-                <svg
-                  className="mr-3 h-5 w-5 animate-spin text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 00-8 8h4l-3 3-3-3h4z"
-                  ></path>
-                </svg>
-                Entrando...
-              </>
-            ) : (
-              'Entrar'
-            )}
-          </Button>
-        </form>
-
-        {/* Salvar senha */}
-        <div className="mt-14 mb-6 z-10 w-full max-w-[600px] px-4">
-          <div className="flex justify-between items-center">
-            <span
-              className={`font-bold ${
-                isDarkMode ? 'text-white' : 'text-black'
-              }`}
-            >
-              Salvar senha
-            </span>
-            <div
-              className="relative cursor-pointer"
-              onClick={() => setIsSaved(!isSaved)}
-            >
-              <Input
-                type="checkbox"
-                className="hidden"
-                checked={isSaved}
-                onChange={() => setIsSaved(!isSaved)}
-              />
-              <div
-                className={`w-12 h-6 pl-1 py-1 rounded-full transition-colors duration-300 ${
-                  isSaved ? 'bg-zinc-950' : 'bg-zinc-300'
-                }`}
-              >
-                <div
-                  className={`w-4 h-4 bg-zinc-50 rounded-full shadow-md transform transition-transform duration-300 ${
-                    isSaved ? 'translate-x-6' : 'translate-x-0'
-                  }`}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Rodapé com alerta */}
         <div
-          className={`z-10 w-full max-w-md px-4 mt-4 border-t-[1.5px] py-6 flex flex-col justify-center items-center gap-6 ${
-            isDarkMode ? 'border-white' : 'border-black'
+          className={`absolute inset-0 z-0 opacity-5 bg-no-repeat bg-cover bg-center ${
+            isDarkMode
+              ? 'bg-[url("/bg_textura_login_escuro.png")]'
+              : 'bg-[url("/bg_textura_login_claro.png")]'
           }`}
-        >
-          <span
-            className={`font-bold text-center ${
-              isDarkMode ? 'text-white' : 'text-black'
-            }`}
-          >
-            Não tem conta? Crie agora!
-          </span>
-          <AlertDialog>
-            <AlertDialogTrigger
-              className={`w-full max-w-[300px] z-10 text-base py-2 cursor-pointer font-bold border-2 duration-300 rounded-md ${
-                isDarkMode
-                  ? 'text-white hover:text-zinc-950 hover:bg-zinc-50 border-white'
-                  : 'text-black hover:text-zinc-50 hover:bg-zinc-950 border-black'
-              }`}
-            >
-              Criar Agora
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Escolha o tipo de cadastro</AlertDialogTitle>
-                <AlertDialogDescription>
-                  Selecione o tipo de conta que você deseja criar.
-                </AlertDialogDescription>
-                <AlertDialogCancel className="absolute right-4 top-4 rounded-sm cursor-pointer">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-4 w-4"
-                  >
-                    <line x1="18" y1="6" x2="6" y2="18"></line>
-                    <line x1="6" y1="6" x2="18" y2="18"></line>
-                  </svg>
-                </AlertDialogCancel>
-              </AlertDialogHeader>
-              <AlertDialogFooter className="flex flex-col gap-2">
-                <Link href="/cadastro/locatario" className="w-full">
-                  <AlertDialogAction className="w-full cursor-pointer">
-                    Locatário
-                  </AlertDialogAction>
-                </Link>
-                <Link href="/cadastro/cliente" className="w-full">
-                  <AlertDialogAction className="w-full cursor-pointer">
-                    Cliente
-                  </AlertDialogAction>
-                </Link>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
-        </div>
+        />
 
         <div className="absolute top-4 right-4">
           <ThemeToggleButton
@@ -304,7 +107,188 @@ export default function Login() {
             toggleTheme={toggleTheme}
           />
         </div>
+
+        <div className="flex items-center gap-4 mb-16 z-10">
+          <Link href="/" className="flex items-center gap-4">
+            <Image
+              src={isDarkMode ? '/icone_branco.png' : '/icone_branco.png'}
+              alt="Logo"
+              width={100}
+              height={100}
+              priority
+              className="lg:w-24 lg:h-24 md:w-20 md:h-20 sm:w-16 sm:h-16 w-16 h-16"
+            />
+            <span className="lg:text-8xl md:text-6xl sm:text-4xl text-4xl font-bold text-white">
+              Login
+            </span>
+          </Link>
+        </div>
+
+        {/* Formulário */}
+        <form onSubmit={handleLogin} className="w-full max-w-[700px] space-y-6">
+          <div className="relative w-full">
+            <User className="absolute w-7 h-7 left-3 top-1/2 transform -translate-y-1/2 text-white" />
+            <Input
+              name="nome_usuario"
+              value={formData.nome_usuario}
+              onChange={handleInputChange}
+              placeholder="Digite aqui seu login!"
+              className="pl-11 py-7 lg:text-base lg:placeholder-white/70 md:lg:placeholder-white/70 sm:lg:placeholder-white/70 text-white md:placeholder-white/70 placeholder-white/70 rounded-lg bg-transparent border border-white lg:focus:border-white lg:focus:ring-0 md:focus:border-white md:focus:ring-0 sm:focus:border-white sm:focus:ring-0 focus:ring-0 focus:border-white"
+              required
+            />
+          </div>
+
+          <div className="relative w-full">
+            <Lock className="absolute w-7 h-7 left-3 top-1/2 transform -translate-y-1/2 text-white" />
+            <Input
+              name="senha"
+              type={showPassword ? 'text' : 'password'}
+              value={formData.senha}
+              onChange={handleInputChange}
+              placeholder="Digite aqui sua senha!"
+              className="pl-11 py-7 lg:text-base lg:placeholder-white/70 md:lg:placeholder-white/70 sm:lg:placeholder-white/70 text-white md:placeholder-white/70 placeholder-white/70 rounded-lg bg-transparent border border-white lg:focus:border-white lg:focus:ring-0 md:focus:border-white md:focus:ring-0 sm:focus:border-white sm:focus:ring-0 focus:ring-0 focus:border-white"
+              required
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"
+            >
+              {showPassword ? <EyeOff size={24} /> : <Eye size={24} />}
+            </button>
+          </div>
+
+          <div className="relative w-full flex justify-center items-center mt-14">
+            <Button
+              type="submit"
+              className="lg:w-96 md:w-80 sm:w-72 w-64 lg:py-7 lg:text-xl md:py-6 md:text-base sm:py-5 sm:text-sm font-bold bg-white text-black hover:bg-zinc-100 cursor-pointer disabled:opacity-100 disabled:bg-white disabled:text-black"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <>
+                  <svg
+                    className="animate-spin h-5 w-5 mr-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    />
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 00-8 8h4l-3 3-3-3h4z"
+                    />
+                  </svg>
+                  Entrando...
+                </>
+              ) : (
+                'Entrar'
+              )}
+            </Button>
+          </div>
+        </form>
+
+        {/* Salvar senha toggle */}
+        <div className="flex justify-between items-center w-full max-w-[700px] mt-14">
+          <span
+            className={`font-bold ${isDarkMode ? 'text-white' : 'text-white'}`}
+          >
+            Salvar senha
+          </span>
+          <div
+            className="relative cursor-pointer"
+            onClick={() => setIsSaved(!isSaved)}
+          >
+            <Input
+              type="checkbox"
+              checked={isSaved}
+              onChange={() => setIsSaved(!isSaved)}
+              className="hidden"
+            />
+            <div
+              className={`w-12 h-6 flex items-center rounded-full p-1 ${
+                isSaved ? 'bg-[#0d4f7d]' : 'bg-white'
+              }`}
+            >
+              <div
+                className={`bg-[#0d4f7d] w-4 h-4 rounded-full shadow-md transform duration-300 ease-in-out ${
+                  isSaved ? 'translate-x-6 bg-white' : ''
+                }`}
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Rodapé */}
+        <div className="flex flex-col items-center gap-4 w-full max-w-[600px] mt-10 border-t pt-6 z-10">
+          <span
+            className={`font-bold ${isDarkMode ? 'text-white' : 'text-white'}`}
+          >
+            Não tem conta? Crie agora!
+          </span>
+          <AlertDialog>
+            <AlertDialogTrigger
+              className={`border-2 px-20 py-3 rounded-md font-bold cursor-pointer ${
+                isDarkMode
+                  ? 'text-white hover:bg-[#161c20] hover:border-transparent'
+                  : 'text-white hover:bg-[#0d4f7d] hover:border-transparent'
+              }`}
+            >
+              Criar Agora
+            </AlertDialogTrigger>
+            <AlertDialogContent
+              className={` ${
+                isDarkMode
+                  ? 'bg-[#161c20] border-[#161c20] text-white'
+                  : 'bg-white border-[#0d4f7d] text-black'
+              }`}
+            >
+              <AlertDialogHeader>
+                <AlertDialogTitle>Escolha o tipo de cadastro</AlertDialogTitle>
+                <AlertDialogDescription>
+                  Selecione o tipo de conta que você deseja criar.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter className="flex flex-col gap-2">
+                <Link href="/cadastro/locatario" className="w-full">
+                  <AlertDialogAction
+                    className={`w-full cursor-pointer ${
+                      isDarkMode
+                        ? 'text-black bg-white hover:bg-gray-50'
+                        : 'text-white bg-black'
+                    }`}
+                  >
+                    Locatário
+                  </AlertDialogAction>
+                </Link>
+                <Link href="/cadastro/cliente" className="w-full">
+                  <AlertDialogAction
+                    className={`w-full cursor-pointer ${
+                      isDarkMode
+                        ? 'text-black bg-white hover:bg-gray-50'
+                        : 'text-white bg-black'
+                    }`}
+                  >
+                    Cliente
+                  </AlertDialogAction>
+                </Link>
+              </AlertDialogFooter>
+              <AlertDialogCancel
+                className={`absolute top-4 right-4 cursor-pointer ${
+                  isDarkMode ? 'text-white bg-[#161c20]' : 'text-black bg-white'
+                }`}
+              >
+                X
+              </AlertDialogCancel>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
